48aa0ab8daf711e5ed8ad01c3aaf6fcf0d999bdf
Neo Lee <neo.lee@gmail.com>
2026-02-22 15:06:00 +0800

    refactor: clean orphan code

--- Mercury/Mercury/SummaryContracts.swift
+++ Mercury/Mercury/SummaryContracts.swift
@@ -11,20 +11,6 @@ nonisolated struct SummarySlotKey: Equatable, Hashable {
     let detailLevel: SummaryDetailLevel
 }
 
-nonisolated enum SummaryWaitingTrigger: Equatable {
-    case manual
-    case auto
-}
-
-nonisolated struct SummaryWaitingDecision: Equatable {
-    let shouldKeepCurrent: Bool
-    let ownersToCancel: [AgentRunOwner]
-}
-
-nonisolated struct SummaryWaitingCleanupDecision: Equatable {
-    let ownersToAbandon: [AgentRunOwner]
-}
-
 nonisolated enum SummaryPolicy {
     static func resolveControlSelection(
         selectedEntryId: Int64,
@@ -72,57 +58,5 @@ nonisolated enum SummaryPolicy {
         guard isSummaryRunning == false else { return false }
         guard hasPersistedSummaryForCurrentEntry == false else { return false }
         return selectedEntryId != nil
-    }
-
-    static func decideWaiting(
-        queuedOwner: AgentRunOwner,
-        queuedTrigger: SummaryWaitingTrigger,
-        displayedEntryId: Int64?,
-        existingWaiting: [AgentRunOwner: SummaryWaitingTrigger]
-    ) -> SummaryWaitingDecision {
-        guard displayedEntryId == queuedOwner.entryId else {
-            return SummaryWaitingDecision(
-                shouldKeepCurrent: false,
-                ownersToCancel: [queuedOwner]
-            )
-        }
-
-        let otherOwners = existingWaiting.keys.filter { $0 != queuedOwner }
-        switch queuedTrigger {
-        case .manual:
-            return SummaryWaitingDecision(
-                shouldKeepCurrent: true,
-                ownersToCancel: Array(otherOwners)
-            )
-        case .auto:
-            let hasOtherManual = existingWaiting.contains { owner, trigger in
-                owner != queuedOwner && trigger == .manual
-            }
-            if hasOtherManual {
-                return SummaryWaitingDecision(
-                    shouldKeepCurrent: false,
-                    ownersToCancel: [queuedOwner]
-                )
-            }
-            return SummaryWaitingDecision(
-                shouldKeepCurrent: true,
-                ownersToCancel: Array(otherOwners)
-            )
-        }
-    }
-
-    static func decideWaitingCleanupOnEntrySwitch(
-        previousEntryId: Int64,
-        nextSelectedEntryId: Int64?,
-        existingWaiting: [AgentRunOwner: SummaryWaitingTrigger]
-    ) -> SummaryWaitingCleanupDecision {
-        guard previousEntryId != nextSelectedEntryId else {
-            return SummaryWaitingCleanupDecision(ownersToAbandon: [])
-        }
-
-        let ownersToAbandon = existingWaiting.keys.filter { owner in
-            owner.taskKind == .summary && owner.entryId == previousEntryId
-        }
-        return SummaryWaitingCleanupDecision(ownersToAbandon: ownersToAbandon)
     }
 }
